daa527dddb3ea837fcd94d18382003dc02731d34
Jeffrey Ke <jke@scu.edu>
2023-03-11 15:52:51 -0800

    Unit test complete Completed unit test for case where object is directly in front of the car. The first assert statement asserts that exactly 28 points that are beneath the safety threshold (arbitrarily set to 10) are detected by the HardwareManager. The thirs assert statement is more involved: I use the np.where() function to get an array of the indices in the test data where there is a lidar range that's less than the safety threshold. I then use list comprehension to construct an array that contains the indices that the hardwaremanager detected a range beneath the threshold. In short, I'm checking that the hardwaremanager correctly found all the ranges below the safety threshold that should be there based on the test array 'obstacle_in_front'

--- src/object_avoidance/src/HardwareManager/HardwareManager.py
+++ src/object_avoidance/src/HardwareManager/HardwareManager.py
@@ -1,2 +1,36 @@
+import numpy as np
+import math
 class HardwareManager():
-    pass
+    def __init__(self, threshold, angleIncrement=0.017541900277137756, startingAngle=-1.5700000524520874):
+        self.threshold = threshold
+        self.angleIncrement = angleIncrement
+        self.startingAngle = startingAngle
+        self.obstaclesArray = []
+
+    def feedLidarRange(self, lidar_ranges):
+        angle = self.startingAngle
+        index = 0
+        for _range in lidar_ranges:
+            
+            if _range < self.threshold:
+                self.obstaclesArray.append((_range, angle, index))
+                angle += self.angleIncrement
+            index = index + 1
+        # self.obstaclesArray = np.array([(_range, self.startingAngle + i * self.angleIncrement) 
+        #                                 for _range, i in enumerate(lidar_ranges)  if _range < self.threshold])
+
+    def getStartingAngle(self):
+        return self.startingAngle
+
+    def numberOfObstaclesDetected(self):
+        return len(self.obstaclesArray)
+    
+    def getObstaclesArray(self):
+        return self.obstaclesArray
+
+    def getSafeDistance(self):
+        return self.threshold
+    
+    def getAngleIncrement(self):
+        return self.angleIncrement
+    
--- src/object_avoidance/test/TestHardwareManager.py
+++ src/object_avoidance/test/TestHardwareManager.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-from src.HardwareManager import HardwareManager
-from sensor_msgs import PointCloud
+from src.HardwareManager.HardwareManager import HardwareManager
 
 def testConstructor():
-    hw_m = HardwareManager(threshold=10, angleIncrment=0.5)
+    hw_m = HardwareManager(threshold=10, angleIncrement=0.5)
     assert(hw_m.getSafeDistance() == 10)
     assert(hw_m.getAngleIncrement() == 0.5)
 
@@ -17,6 +16,11 @@ def testLidarObstacleFront():
 
     assert(hw_m.numberOfObstaclesDetected() == 28)
     assert(len(hw_m.getObstaclesArray()) == 28)
-    assert (np.where(obstacle_in_front < hw_m.getSafeDistance()) == np.array([index[1]/hw_m.getAngleIncrement() for index in hw_m.getObstaclesArray()]))
+    print(np.where(obstacle_in_front < hw_m.getSafeDistance())[0])
+    # print(hw_m.getObstaclesArray())
+    print(np.array([tup[2] for tup in hw_m.getObstaclesArray()]))
+    assert(np.all(np.where(obstacle_in_front < hw_m.getSafeDistance())[0] == np.array([tup[2] for tup in hw_m.getObstaclesArray()])))
 
-
+    # This is a test of the functionality where every time the hw_m is fed LiDAR ranges, it creates
+    # an array of tuples of the format (range, angle), where range is the distance detected that's
+    # beneath the threshold, and angle is at the angle where that range was detected. 
