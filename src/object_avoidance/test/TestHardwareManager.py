88c59e43903914993a7cebe15e5fef019482827f
Jeffrey Ke <jke@scu.edu>
2023-03-11 14:26:28 -0800

    First few unit tests Testing how the HardwareManager responds to being fed LiDAR range output

--- src/object_avoidance/test/TestHardwareManager.py
+++ src/object_avoidance/test/TestHardwareManager.py
@@ -1,4 +1,22 @@
+import numpy as np
+
 from src.HardwareManager import HardwareManager
+from sensor_msgs import PointCloud
 
 def testConstructor():
-    assert(True)
+    hw_m = HardwareManager(threshold=10, angleIncrment=0.5)
+    assert(hw_m.getSafeDistance() == 10)
+    assert(hw_m.getAngleIncrement() == 0.5)
+
+def testLidarObstacleFront():
+
+    hw_m = HardwareManager(threshold=10)
+    from LaserScanTestData import obstacle_in_front
+    obstacle_in_front = np.array(obstacle_in_front)
+    hw_m.feedLidarRange(obstacle_in_front)
+
+    assert(hw_m.numberOfObstaclesDetected() == 28)
+    assert(len(hw_m.getObstaclesArray()) == 28)
+    assert (np.where(obstacle_in_front < hw_m.getSafeDistance()) == np.array([index[1]/hw_m.getAngleIncrement() for index in hw_m.getObstaclesArray()]))
+
+
