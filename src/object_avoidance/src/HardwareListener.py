c476039a868b64da2cd7cf4d684a953523e4081a
Jeffrey Ke <jke@scu.edu>
2023-03-12 14:39:42 -0700

    Building hardwareListener.py

--- src/object_avoidance/src/HardwareListener.py
+++ src/object_avoidance/src/HardwareListener.py
@@ -1,15 +1,31 @@
 #! /usr/bin/python3
 
 import rospy
-from std_msgs.msg import String
-from HardwareManager import HardwareManager
+from sensor_msgs.msg import LaserScan
+from object_avoidance.msg import RepulsionVector
+from object_avoidance.msg import RepulsionVectorArray
+from HardwareManager.HardwareManager import HardwareManager
 
-def LidarQuery():
+hw_m = HardwareManager(threshold=10)
 
-    rospy.init_node('hw_manager')
+def feedManager(data: LaserScan):
+    global hw_m
+    hw_m.angleIncrement = data.angle_increment
+    hw_m.startingAngle = data.angle_min
+    hw_m.feedLidarRange(data.ranges)
 
-    rospy.Subscriber("/LIDAR", String, )
+    # obstacleVectors = hw_m.getObstacleVectors()
+    # pub.publish(obstacleVectiors)
+    pass
+
+def hardwareListener():
+
+    rospy.init_node('hw_listener')
+
+    rospy.Subscriber("/catvehicle/front_laser_points", LaserScan, feedManager)
+    # pub = rospy.Publisher('/catvehicle/obstacle_repulsion', RepulsionVectorArray)
+    rospy.spin()
 
 
 if __name__ == '__main__':
-    LidarQuery()
+    hardwareListener()
