caf3a90601be2cc751a255bb4fb57eef244b3dbf
jeff <jke@scu.edu>
2023-03-14 15:12:45 -0700

    Emperical tuning of error margins and Kp.

--- src/object_avoidance/src/CourseCorrector/CourseCorrector.py
+++ src/object_avoidance/src/CourseCorrector/CourseCorrector.py
@@ -2,11 +2,12 @@ import math
 
 class CourseCorrector():
     
-    def __init__(self, K_l = 2.0, K_a = 2.0):
+    def __init__(self, K_l = 2.0, K_a = 5.0):
         self.point = (0.0,0.0,0.0)
         self.heading = (0.0,0.0,0.0)
         self.waypoint = (0.0,0.0,0.0)
-        self.error_margin = 0.01
+        self.angle_margin = 0.01
+        self.destination_margin = 5.0
         self.K_l = K_l
         self.K_a = K_a
 
@@ -40,10 +41,14 @@ class CourseCorrector():
         angular = (0, 0, 0)
         h_error = self.getHeadingError()
         l_error = self.getDistanceToWaypoint()
-        if (h_error > self.error_margin):
-            angular = (0, 0, self.K_a * h_error)
-            linear = (1.0, 0, 0)
-        elif (l_error > self.error_margin):
-            linear = (self.K_l * l_error, 0, 0)
+        l_error = 10.0 if l_error > 10.0 else l_error
+        if (l_error > self.destination_margin):
+            print("Heading error: ", h_error)
+            if (abs(h_error) > self.angle_margin):
+                print("turning...")
+                angular = (0, 0, self.K_a * h_error)
+                linear = (2.0, 0, 0)
+            else:
+                linear = (self.K_l * l_error, 0, 0)
 
         return (linear, angular)
