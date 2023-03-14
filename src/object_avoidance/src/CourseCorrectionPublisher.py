c9f74422699203b0ee2b155a7e3bb2f45c56a1c1
jeff <jke@scu.edu>
2023-03-14 14:04:54 -0700

    Logging statements for monitoring heading

--- src/object_avoidance/src/CourseCorrectionPublisher.py
+++ src/object_avoidance/src/CourseCorrectionPublisher.py
@@ -4,21 +4,25 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 from nav_msgs.msg import Odometry
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
+import math
 
-course_linear = Vector3(0.0, 0.0, 0.0)
-course_angular = Vector3(0.0, 0.0, 1.0)
+course_linear = Vector3(0.5, 0.0, 0.0)
+course_angular = Vector3(0.0, 0.0, 10.0)
 
 def reportPose(data: Odometry):
     orientation = data.pose.pose.orientation
     print("Quaternion pose: x: {} y: {} z: {} w: {}".format(orientation.x, orientation.y, orientation.z, orientation.w))
     explicit_quat = [orientation.x, orientation.y, orientation.z, orientation.w]
+    pitch, roll, yaw = euler_from_quaternion(explicit_quat)
     print("Euler pose: {}".format(euler_from_quaternion(explicit_quat)))
+    if(yaw > math.pi):
+        course_angular.z = 0.0
 
 
 def main():
     global course_linear, course_angular
     rospy.init_node('CourseCorrectionPublisher')
-    pub = rospy.Publisher('/catvehicle/cmd_vel', Twist, queue_size=10)
+    pub = rospy.Publisher('/catvehicle/cmd_vel_safe', Twist, queue_size=10)
     rospy.Subscriber('/catvehicle/odom', Odometry, reportPose)
     rate = rospy.Rate(100)
     while not rospy.is_shutdown():
