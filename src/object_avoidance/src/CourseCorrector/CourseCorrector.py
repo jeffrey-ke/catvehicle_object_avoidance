1bdaf2069299579a451d28034c7d94140e24c9ee
jeff <jke@scu.edu>
2023-03-14 16:34:20 -0700

    Verified waypoint stack

--- src/object_avoidance/src/CourseCorrector/CourseCorrector.py
+++ src/object_avoidance/src/CourseCorrector/CourseCorrector.py
@@ -5,11 +5,12 @@ class CourseCorrector():
     def __init__(self, K_l = 2.0, K_a = 5.0):
         self.point = (0.0,0.0,0.0)
         self.heading = (0.0,0.0,0.0)
-        self.waypoint = (0.0,0.0,0.0)
+        self.waypoint = []
         self.angle_margin = 0.01
-        self.destination_margin = 5.0
+        self.destination_margin = 2.0
         self.K_l = K_l
         self.K_a = K_a
+        self.l_saturation = 10.0
 
 
     def givePose(self, point, heading=None):
@@ -18,16 +19,21 @@ class CourseCorrector():
             self.heading = heading
 
     def giveWaypoint(self, point):
-        self.waypoint = point
+        self.waypoint.append(point)
     
     def getDistanceToWaypoint(self):
-        return math.sqrt (math.pow(self.waypoint[0] - self.point[0], 2)
-                          + math.pow(self.waypoint[1] - self.point[1], 2)
-                          + math.pow(self.waypoint[2] - self.point[2], 2))
+        currentWaypoint = self.waypoint[-1]
+        print()
+        error = math.sqrt (math.pow(currentWaypoint[0] - self.point[0], 2)
+                          + math.pow(currentWaypoint[1] - self.point[1], 2)
+                          + math.pow(currentWaypoint[2] - self.point[2], 2))
+        error = error if error < self.l_saturation else self.l_saturation #saturation
+        return error
     
     def getHeadingError(self):
-        desired_heading = math.atan2 (self.waypoint[1] - self.point[1], 
-                                      self.waypoint[0] - self.point[0])
+        currentWaypoint = self.waypoint[-1]
+        desired_heading = math.atan2 (currentWaypoint[1] - self.point[1], 
+                                      currentWaypoint[0] - self.point[0])
         error = desired_heading - self.heading[2] #only return error in yaw
 
         while (error < -math.pi):
@@ -36,16 +42,22 @@ class CourseCorrector():
             error = error - 2 * math.pi
         return error
     
+    def checkIfArrived(self):
+        l_error = self.getDistanceToWaypoint()
+        if (l_error < self.destination_margin):
+            if(len(self.waypoint) > 1):
+                self.waypoint.pop()
+
     def getCommandVelocity(self):
         linear = (0, 0, 0)
         angular = (0, 0, 0)
+
+        self.checkIfArrived()
+
         h_error = self.getHeadingError()
         l_error = self.getDistanceToWaypoint()
-        l_error = 10.0 if l_error > 10.0 else l_error
-        if (l_error > self.destination_margin):
-            print("Heading error: ", h_error)
+        if (l_error > self.destination_margin): #car has arrived
             if (abs(h_error) > self.angle_margin):
-                print("turning...")
                 angular = (0, 0, self.K_a * h_error)
                 linear = (2.0, 0, 0)
             else:
--- src/object_avoidance/test/test_CourseCorrection.py
+++ src/object_avoidance/test/test_CourseCorrection.py
@@ -34,7 +34,7 @@ def testVelocityWhenHeadingError():
     c.giveWaypoint(endPoint)
     #Using default start point and heading
 
-    correct_linear = (0, 0, 0)
+    correct_linear = (2, 0, 0)
     correct_angular = (0, 0, c.K_a * c.getHeadingError())
     assert (c.getCommandVelocity() == (correct_linear, correct_angular))
 
@@ -55,4 +55,30 @@ def testVelocityNoHeadingError():
     correct_linear = (c.K_l * c.getDistanceToWaypoint(), 0, 0)
     correct_angular = (0, 0, 0)
     assert (c.getCommandVelocity() == (correct_linear, correct_angular))
+
+def testWaypointTransitions():
+    c = CourseCorrector()
+
+    final_destination = (50.0, 50.0, 0.0)
+    c.giveWaypoint(final_destination)
+
+    first_stop = (30.0, -30,0, 0.0)
+    c.giveWaypoint(first_stop)
+
+    correct_linear = (2, 0, 0)
+    correct_angular = (0, 0, c.K_a * c.getHeadingError())
+    assert (c.getCommandVelocity() == (correct_linear, correct_angular))
+
+
+    c.givePose(first_stop) #car has arrived to the first stop; now we expect it to head to the next waypoint
+
+    cmd_vel = c.getCommandVelocity()
+    correct_linear = (2, 0, 0)
+    correct_angular = (0, 0, c.K_a * c.getHeadingError())
+    assert (cmd_vel == (correct_linear, correct_angular))
+    assert (c.waypoint[-1] == (50.0, 50.0, 0.0))
+
+
+
+
     
