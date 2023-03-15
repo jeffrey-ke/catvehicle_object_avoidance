c86f8da2fef530d54acf6a11c6c3609fc8e6f984
jeff <jke@scu.edu>
2023-03-14 21:08:40 -0700

    Added ObstacleArray custom msg type

--- src/object_avoidance/src/HardwareListener.py
+++ src/object_avoidance/src/HardwareListener.py
@@ -2,11 +2,10 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
-from object_avoidance.msg import RepulsionVectorArray
+from object_avoidance.msg import ObstacleArray, Obstacle
 from HardwareManager.HardwareManager import HardwareManager
 from nav_msgs.msg import Odometry
 import numpy as np
-from geometry_msgs.msg import Vector3
 
 hw_m = HardwareManager(threshold=10)
 carPos = [0,0,0]
@@ -18,24 +17,29 @@ def feedManager(data: LaserScan):
     hw_m.feedLidarRange(data.ranges)
     pass
 
-def updateCarPos(data: Odometry):
-    global carPos
-    pose = data.pose.pose.position
-    carPos = [pose.x, pose.y, pose.z]
-    pass
+# def updateCarPos(data: Odometry):
+#     global carPos
+#     pose = data.pose.pose.position
+#     carPos = [pose.x, pose.y, pose.z]
+#     pass
 
-def hardwareListener():
+def main():
 
     rospy.init_node('hw_listener')
     rospy.Subscriber("/catvehicle/front_laser_points", LaserScan, feedManager)
-    rospy.Subscriber("catvehicle/odom", Odometry, updateCarPos)
+    # rospy.Subscriber("catvehicle/odom", Odometry, updateCarPos)
+    pub = rospy.Publisher("/catvehicle/obstacles", ObstacleArray, queue_size=10)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
+        msg = ObstacleArray()
+        msg.count = hw_m.numberOfObstaclesDetected()
+        msg.obstacles = [Obstacle(_range, angle) for _range, angle, index in hw_m.getObstaclesArray()]
+        pub.publish(msg)
         rate.sleep()
 
 
 if __name__ == '__main__':
     try: 
-        hardwareListener()
+        main()
     except rospy.ROSInterruptException:
         pass
